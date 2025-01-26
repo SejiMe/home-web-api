4c92c539817e932f6a7662bd8bbb80f25f7c2d87
SejiMe <atienza.seji@gmail.com>
2025-01-26 13:46:44 +0800

    project initialization

---
+++ src/web-api/Common/Persistence/Options/ConnectionOptions.cs
@@ -0,0 +1,7 @@
+namespace home.web.api.Common.Persistence.Options
+{
+    public class ConnectionStringsOptions
+    {
+        public string HomeDb { get; set; }
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace home.web.unit.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+}
