ac3b7edaff3820057a0c251903c6149e36158cc3
SejiMe <atienza.seji@gmail.com>
2025-02-02 06:11:51 +0800

    dev: adjust logic on Methods to ensure proper complete and incomeplete

--- src/web-api/Models/Todo.cs
+++ src/web-api/Models/Todo.cs
@@ -1,5 +1,6 @@
 namespace web.api.Models
 {
+    ///
     public class Todo
     {
         public Guid Id { get; set; } = Guid.CreateVersion7();
@@ -10,11 +11,19 @@ namespace web.api.Models
 
         public void CompleteTask()
         {
+            if (IsComplete is true)
+            {
+                throw new InvalidOperationException("Task is already completed");
+            }
+
             IsComplete = true;
         }
 
         public void IncompleteTask()
         {
+            if (IsComplete is not true)
+                throw new InvalidOperationException("Task is already incomplete");
+
             IsComplete = false;
         }
     }
